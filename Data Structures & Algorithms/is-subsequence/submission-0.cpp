35feb689450fdf9dce14ede5bdd2864f96a4b183
Gunda Harshavardhan <149779783+Harsha-AIE@users.noreply.github.com>
2026-07-19 17:26:23 +0530

    Add: is-subsequence - submission-0

---
+++ Data Structures & Algorithms/is-subsequence/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0,j=0;
+        while(i<s.size()){
+            if(j>=t.size()){
+                return false;
+            }
+            if(s[i]==t[j]){
+                i++;
+                
+            }j++;
+        }
+        return true;
+    }
+};
